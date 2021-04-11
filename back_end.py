ce36e93cc3ac4c3f4099d7bff0d392a886614eba
hzc011213 <hzc011213@gmail.com>
2021-04-11 15:06:11 +0800

    Update back_end.py

--- back_end.py
+++ back_end.py
@@ -2,4 +2,14 @@
 This file contains the necessary game objects, their classes and functions
 associated.
 """
-sadhsid
+<<<<<<< HEAD
+
+
+
+class Merge:
+    """
+    Merge all classes we used.
+    """
+    player: Player()
+    object_set: set(Object)
+
