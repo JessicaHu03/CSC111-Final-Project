d573427cbd013eb8631da8a9cefb33ac4e6e090b
Archivec <shijun.lin@leadvisor.com>
2021-04-11 20:29:14 +0800

    Modified objects

---
+++ objects.py
@@ -0,0 +1,6 @@
+"""
+This function contains all the relevant objects seen in the game. This includes:
+    - Treasures and Keys
+    - Obstacles
+Represented as classes, with their corresponding methods and attributes.
+"""
