6b87334fc2271a7ec7c42e03df75bd74e1daecbb
Archivec <shijun.lin@leadvisor.com>
2021-04-12 00:14:24 +0800

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,5 +1,5 @@
 """
-This function contains all the relevant objects seen in the game. This includes:
+This function contains all the relevant objects seen in the game. This includes
     - Treasures and Keys
     - Obstacles
 Represented as classes, with their corresponding methods and attributes.
